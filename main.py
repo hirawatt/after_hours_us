6115f6776b7021a45315dbb4e7268657cfb308f8
VH <hirawatt@pm.me>
2021-02-05 17:44:07 +0530

    Added Streamlit Support

--- main.py
+++ main.py
@@ -5,38 +5,45 @@ import yfinance as yf
 from yahoo_fin import stock_info as si
 import streamlit as st
 
-# Get After Hours Data from Source
-after_hours_url = 'https://www.marketwatch.com/tools/screener/after-hours'
+st.title('After Hours Volume as a Percentage of Float')
 
-# Symbol     Company Name    After HoursPrice    After HoursVol      After HoursChg      After HoursChg %
-df = pd.read_html(after_hours_url)[2]
+#@st.cache(suppress_st_warning=True)
+def after_hours_usa():
+    # Get After Hours Data from Source
+    after_hours_url = 'https://www.marketwatch.com/tools/screener/after-hours'
 
-tickers = df['Symbol'].tolist()
+    # Symbol     Company Name    After HoursPrice    After HoursVol      After HoursChg      After HoursChg %
+    df = pd.read_html(after_hours_url)[2]
+    st.dataframe(df)
 
-#  Company Name      After HoursVol
-data_df = df[['Company Name', 'After HoursVol']]
-print(data_df)
+    # List of Ticker Symbols
+    tickers = df['Symbol'].tolist()
 
-display(tickers)
-# Stock Symbol
-df0 = pd.DataFrame(tickers, columns=['Stock Symbol'])
+    #  Company Name      After HoursVol
+    data_df = df[['Company Name', 'After HoursVol']]
 
-# Initializing DataFrame
-df2 = pd.DataFrame()
+    # Stock Symbol
+    df0 = pd.DataFrame(tickers, columns=['Stock Symbol'])
 
-# Get Data For all the Tickers
-for i in tickers:
-    df = si.get_stats(i)
-    df1 = df.loc[df['Attribute'] == 'Float']
-    df2 = df2.append(df1, ignore_index=True)
+    # Initializing DataFrame
+    df2 = pd.DataFrame()
 
-# Rename and Drop Columns of DataFrame
-df2 = df2.rename(columns={"Value":"Float Shares"}, errors="raise")
-df2 = df2.drop(columns=['Attribute'])
+    # Get Data For all the Tickers
+    for i in tickers:
+        df = si.get_stats(i)
+        df1 = df.loc[df['Attribute'] == 'Float']
+        df2 = df2.append(df1, ignore_index=True)
 
-# Float Shares      Symbol
-df3 = df2.join(df0)
+    # Rename and Drop Columns of DataFrame
+    df2 = df2.rename(columns={"Value":"Float Shares"}, errors="raise")
+    df2 = df2.drop(columns=['Attribute'])
 
-# Company Name      After HoursVol      Float Shares        Stock Symbol
-final_df = data_df.join(df3)
-print(final_df)
+    # Float Shares      Symbol
+    df3 = df2.join(df0)
+
+    # Company Name      After HoursVol      Float Shares        Stock Symbol
+    final_df = data_df.join(df3)
+    return final_df
+
+my_dataframe = after_hours_usa()
+st.dataframe(my_dataframe)
